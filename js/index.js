34cad5b1bccaf7e1ee885792256ca86973c95ca0
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-01 12:02:09 -0700

    adding search on enter functionality, styling

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 const img = document.querySelector('img');
-fetch(
-  'https://api.giphy.com/v1/gifs/translate?api_key=fOP1i89KgZLAL2lcu3PMoQ1Q7YUyMGK9&s=dogs',
-  { mode: 'cors' }
-)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url;
-  });
+const box = document.querySelector('input');
+box.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    console.log(box.value);
+    fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=fOP1i89KgZLAL2lcu3PMoQ1Q7YUyMGK9&s=${box.value}`,
+      { mode: 'cors' }
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        img.classList.remove('hide');
+        img.classList.add('show');
+        img.src = response.data.images.original.url;
+      })
+      .catch((e) => {
+        alert('Sorry, try again!');
+      });
+  }
+});
