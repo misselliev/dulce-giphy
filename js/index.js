4a525cbf067d1e0de737e50c606c7d9060e6432d
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-01 12:18:25 -0700

    accesslint

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const img = document.querySelector('img');
 const box = document.querySelector('input');
+
 box.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
-    console.log(box.value);
     fetch(
       `https://api.giphy.com/v1/gifs/translate?api_key=fOP1i89KgZLAL2lcu3PMoQ1Q7YUyMGK9&s=${box.value}`,
       { mode: 'cors' }
